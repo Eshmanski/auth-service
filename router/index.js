49953b13fe1ed3c6afc5bdcb69bd63c261b68c8a
Pavel Eshmanski <Eshmanski@gmail.com>
2025-02-05 16:56:12 +0300

    Fix: fix email case sensitivity

--- router/index.js
+++ router/index.js
@@ -1,12 +1,25 @@
 const personController = require('../controllers/person');
+const emailToLowerCase = require('../middlewares/email');
 const authMiddlewares = require('../middlewares/auth');
 const { body } = require('express-validator');
 const Router = require('express');
 const router = Router();
 
-router.post('/registration', body('email').isEmail(), body('password').isLength({ min: 6, max: 32 }), personController.registration);
+router.post(
+    '/registration',
+    emailToLowerCase,
+    body('email').isEmail(),
+    body('password').isLength({ min: 6, max: 32 }),
+    personController.registration
+);
+
+router.post(
+    '/login',
+    emailToLowerCase,
+    personController.login
+);
+
 router.post('/logout', personController.logout);
-router.post('/login', personController.login);
 
 router.get('/persons', authMiddlewares, personController.getPersons);
 router.get('/activate/:link', personController.activate);
